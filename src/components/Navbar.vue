76da609f5c4e21f58e0ea22f4eeb0470c102b4b1
Maxence Lavenu <50020222+maxencelav@users.noreply.github.com>
2020-09-30 22:50:46 +0200

    Added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar-brand to="/">
+        <b-icon-cloud-fill></b-icon-cloud-fill> weather-dashboard
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">Recherche</b-nav-item>
+        <b-nav-item to="/about">À propos</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
