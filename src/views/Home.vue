4288ee01de9460dae14ab604e173077f771c28a4
Maxence Lavenu <maxence.lavenu@outlook.fr>
2020-09-30 15:49:08 +0200

    Search working WIP (lots of API calls)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <b-container>
-    <b-row class="vh-90" align-v="center">
+    <b-row class='vh-90' align-v='center'>
       <b-col sm='12'><CitySearch></CitySearch> </b-col>
-      <b-col ><CityDisplay :city="currentCity"></CityDisplay></b-col>
+      <b-col><CityDisplay :city='currentCity'></CityDisplay></b-col>
+      <b-col>{{ currentCity }}</b-col>
     </b-row>
   </b-container>
 </template>
@@ -14,16 +15,22 @@ import CityDisplay from '@/components/CityDisplay.vue'
 
 export default {
   name: 'Home',
+  computed: {
+    currentCity: {
+      get () {
+        return this.$store.getters.userCityInput
+      }
+    }
+  },
   data () {
     return {
       // city gets automatically loaded ONLY on page load
-      currentCity: this.$store.getters.userCityInput
+      // currentCity: this.$store.getters.userCityInput
     }
   },
   components: {
     CitySearch,
     CityDisplay
   }
-
 }
 </script>
