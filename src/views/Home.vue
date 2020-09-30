a9b4ea287805992a792f9a76ecd4323abdd80f6b
Maxence Lavenu <50020222+maxencelav@users.noreply.github.com>
2020-09-30 20:47:26 +0200

    Start of the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
   <b-container fluid="md">
-    <b-row class='vh-100 align-content-center'>
-      <b-col sm='12'><CitySearch></CitySearch></b-col>
-      <b-col><CityDisplay :city='currentCity'></CityDisplay></b-col>
-      <b-col>{{ currentCity }}</b-col>
+    <b-row class="align-content-center">
+      <b-col sm="12">
+        <h1>Météo</h1>
+        <p class="lead">
+          Récupérez les données météo du lieu que vous souhaitez, et affichez jusqu'à 10 lieux simultanément.
+        </p>
+      </b-col>
+      <b-col sm="12"><CitySearch></CitySearch></b-col>
+      <b-col sm="12" class="mt-4"
+        ><CityDisplay :city="currentCity"></CityDisplay
+      ></b-col>
     </b-row>
   </b-container>
 </template>
