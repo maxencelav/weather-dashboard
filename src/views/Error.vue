34934c9f8aef6714546555a5d27dcecb37033151
Maxence Lavenu <50020222+maxencelav@users.noreply.github.com>
2020-10-02 00:37:49 +0200

    New error and About page

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,6 +1,9 @@
 
 <template>
   <div class="error">
-    <h1>Une erreur est survenue. 😔</h1>
+    <b-col md="12" class="mt-5 text-center">
+      <span class="display-1 d-block">404</span>
+      <div class="mb-4 lead">Une erreur est survenue. 🤔</div>
+    </b-col>
   </div>
 </template>
